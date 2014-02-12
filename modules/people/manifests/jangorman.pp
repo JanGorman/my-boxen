f4ca555cb31d0bf038ec09d05d0e1e1ef4341bac
Jan Gorman <gorman.jan@gmail.com>
2014-02-12 08:30:58 +0100

    comment out wrong config

--- modules/people/manifests/jangorman.pp
+++ modules/people/manifests/jangorman.pp
@@ -1,39 +1,50 @@
-include poeple::jangorman::applications
+include people::jangorman::applications
 
 class people::jangorman {
   
-  notify { 'class people::jangorman declared': }
-
-  # Changes the default shell to the zsh version we get from Homebrew
-  # Uses the osx_chsh type out of boxen/puppet-osx
-  osx_chsh { $::luser:
-    shell   => '/opt/boxen/homebrew/bin/zsh',
-    require => Package['zsh']
-  }
+  $home     = "/Users/${::boxen_user}"
+  $my       = "${home}/my"
+  $dotfiles = "${my}/dotfiles"
   
-  file_line { 'add zsh to /etc/shells':
-    path    => '/etc/shells',
-    line    => "${boxen::config::homebrewdir}/bin/zsh",
-    require => Package['zsh']
-  }
-  
-  ##################################
-  ## Facter, Puppet, and Envpuppet##
-  ##################################
-
-  repository { "${::boxen_srcdir}/puppet":
-    source => 'puppetlabs/puppet'
+  file { $my:
+    ensure  => directory
   }
 
-  repository { "${::boxen_srcdir}/facter":
-    source => 'puppetlabs/facter'
+  repository { $dotfiles:
+    source  => 'jbarnette/dotfiles',
+    require => File[$my]
   }
 
-  file { '/bin/envpuppet':
-    ensure  => link,
-    mode    => '0755',
-    target  => "${::boxen_srcdir}/puppet/ext/envpuppet",
-    require => Repository["${::boxen_srcdir}/puppet"]
-  }
+  # # Changes the default shell to the zsh version we get from Homebrew
+  # # Uses the osx_chsh type out of boxen/puppet-osx
+  # osx_chsh { $::luser:
+  #   shell   => '/opt/boxen/homebrew/bin/zsh',
+  #   require => Package['zsh']
+  # }
+  # 
+  # file_line { 'add zsh to /etc/shells':
+  #   path    => '/etc/shells',
+  #   line    => "${boxen::config::homebrewdir}/bin/zsh",
+  #   require => Package['zsh']
+  # }
+  # 
+  # ##################################
+  # ## Facter, Puppet, and Envpuppet##
+  # ##################################
+  # 
+  # repository { "${::boxen_srcdir}/puppet":
+  #   source => 'puppetlabs/puppet'
+  # }
+  # 
+  # repository { "${::boxen_srcdir}/facter":
+  #   source => 'puppetlabs/facter'
+  # }
+  # 
+  # file { '/bin/envpuppet':
+  #   ensure  => link,
+  #   mode    => '0755',
+  #   target  => "${::boxen_srcdir}/puppet/ext/envpuppet",
+  #   require => Repository["${::boxen_srcdir}/puppet"]
+  # }
 
 }
